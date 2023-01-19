13875ec1c3ef590edd7f00b5c5f4fcb274700b1e
cdonohue@hotmail.com <charles.donohue@gameloft.com>
2023-01-19 10:27:03 -0500

    Batch file runs without using launchSettings.json

--- templates/dotnetscript/dotnetscript.bat
+++ templates/dotnetscript/dotnetscript.bat
@@ -1,4 +1,4 @@
 @echo off
 PUSHD "%~dp0"
-dotnet run --project dotnetscript.csproj -- --verbose --timestamps --scopes --scopetimings %*
+dotnet run --project dotnetscript.csproj --no-launch-profile -- --verbose --timestamps --scopes --scopetimings %*
 POPD
